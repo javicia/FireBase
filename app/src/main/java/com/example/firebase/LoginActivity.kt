f31f79b074e4a0789c8e15c2673bd332d04d6336
javicia <javier.garcia.perez@hotmail.com>
2023-02-16 14:38:56 +0100

    buble

--- app/src/main/java/com/example/firebase/LoginActivity.kt
+++ app/src/main/java/com/example/firebase/LoginActivity.kt
@@ -2,10 +2,19 @@ package com.example.firebase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.ktx.Firebase
 
+//Splash
+//Thread.sleep(2000) //HACK:
+//setTheme(R.style.AppTheme)
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        val analytics = FirebaseAnalytics.getInstance(this)
+        val bundle = Bundle()
+        bundle.putString("message","Integración con Firebase completa")
+        analytics.logEvent("InitScreen",bundle)
     }
 }
